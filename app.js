a52a19371849e3b37655301ac7875d111ad1c7e9
Rig135 <167835509+Rig135@users.noreply.github.com>
2025-01-14 11:57:14 +0530

    fix bugs regarding multiple operators

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ const divide = function (a, b) {
 
 
 let expression = '';
+let result = 0;
+
 
 const clear = document.querySelector('#clear');
 const display = document.querySelector('#display');
@@ -24,6 +26,7 @@ const equal = document.querySelector('#equal');
 clear.addEventListener('click', () => {
     display.textContent = '';
     expression = '';
+    result = 0;
 })
 
 
@@ -41,6 +44,17 @@ const operators = document.querySelectorAll('.operator');
 operators.forEach((operator) => {
     operator.addEventListener('click', (event) => {
         let element = event.target;
+
+        if (element.textContent === '-') {
+            if (expression === '') {
+                expression = '0' + expression;
+                display.textContent += '0';
+            }
+        }
+        if (expression.includes('+') || expression.includes('-') || expression.includes('x') || expression.includes('÷')) {
+            evalExpression();
+
+        }
         display.textContent += element.textContent;
         expression += ` ${element.textContent} `;
         console.log(expression);
@@ -49,15 +63,36 @@ operators.forEach((operator) => {
 
 
 equal.addEventListener('click', () => {
+    evalExpression();
+})
+
+const evalExpression = function () {
+    console.log(expression);
     const token = expression.split(' ');
     console.log(token);
     let result = parseInt(token[0]);
+    if (isNaN(result)) {
+        result = "ERROR!";
+    }
 
     for (let i = 1; i < token.length; i += 2) {
         const operator = token[i];
         const nextNumber = parseInt(token[i + 1]);
 
+        if (isNaN(nextNumber)) {
+            result = 'ERROR!';
+            break;
+        }
+
         result = operate(result, operator, nextNumber);
+        if (result === Infinity || result === -Infinity) {
+            result = "Don't divide by 0";
+            break;
+        }
+        if (result % 1 !== 0) {
+            result = result.toFixed(2);
+        }
+
         if (isNaN(result)) {
             result = 'ERROR!';
             break;
@@ -67,7 +102,7 @@ equal.addEventListener('click', () => {
 
     display.textContent = result;
     expression = result.toString();
-})
+}
 
 const operate = function (a, sign, b) {
     switch (sign) {
